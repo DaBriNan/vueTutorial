013d1af9a15fe3160ea2685e418c0c37add513d6
DaBriNan <138089746+DaBriNan@users.noreply.github.com>
2025-06-23 12:48:54 -0600

    tareas

---
+++ src/components/ListaDeTareas.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h2> Lista de tareas</h2>
+        <input type="text" v-model="nuevaTarea" @keyup.enter="addTarea">
+        <ul>
+            <li v-for="(tarea,index) in tarea" :key="index">
+                {{ tarea }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup> 
+
+import {ref} from 'vue';
+const tarea=ref([]);
+const nuevaTarea=ref('');
+const addTarea= () =>{
+    tarea.value.push(nuevaTarea.value); //v-model manda llamar la variable reactiva
+    nuevaTarea.value='';//cuando lo escribimos se resetea
+}
+
+</script>
+
+<style>
+</style>
